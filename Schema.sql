b9e38436ff95914acebcb984edfe9c1ebb8f45a7
shivalisahai <shivali.sahai@gmail.com>
2022-06-27 10:22:44 -0400

    C:/Program Files/Git/message

--- Schema.sql
+++ Schema.sql
@@ -35,3 +35,24 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+
+CREATE TABLE dept_emp (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(20) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+  PRIMARY KEY (emp_no)
+);
